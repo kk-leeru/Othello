efb8883cf8001d51dfa9cf6f5a1ade64a8fcf5c2
kk-leeru <86719231+kk-leeru@users.noreply.github.com>
2021-06-30 22:20:10 +0800

    Add files via upload
    Beat baseline 1,2,3,5 except 4 (depth 3, alpha beta prune)

--- kk.leeru.cpp
+++ kk.leeru.cpp
@@ -214,10 +214,14 @@ void write_valid_spot(std::ofstream& fout) {
 //        fout << p.x << " " << p.y << std::endl;
 //        fout.flush();
 //    }
-
+    int n_valid_spots = next_valid_spots.size();
+    srand(time(NULL));
+    // Choose random spot. (Not random uniform here)
+    int index = (rand() % n_valid_spots);
+    Point p = next_valid_spots[index];
+    fout << p.x << " " << p.y << std::endl;
     int max_ns = -1000000;
     int beta = 1000000;
-    Point p;
     for (auto c: next_valid_spots){
         int value = 0;
         OthelloBoard g_cpy;
@@ -246,9 +250,9 @@ int minimax(OthelloBoard st, int depth, bool maximizingPlayer, int alpha, int be
             g_cpy.put_disc(c);
             value = max(value, minimax(g_cpy, depth-1, false, alpha, beta));
             alpha = max(alpha, value);
-//            if (alpha >= beta){
-//                break;
-//            }
+            if (alpha >= beta){
+                break;
+            }
         }
         //return alpha;
         return value;
@@ -260,9 +264,9 @@ int minimax(OthelloBoard st, int depth, bool maximizingPlayer, int alpha, int be
             g_cpy.put_disc(c);
             value = min(value, minimax(g_cpy, depth-1, true, alpha, beta));
             beta = min(beta, value);
-//            if (beta <= alpha){
-//                break;
-//            }
+            if (beta <= alpha){
+                break;
+            }
         }
         //return beta;
         return value;
@@ -271,6 +275,11 @@ int minimax(OthelloBoard st, int depth, bool maximizingPlayer, int alpha, int be
 }
 int state(OthelloBoard st)
 {
+    std::vector<Point> stable = {Point(0,0), Point(0,7), Point(7,0), Point(7,7)}; //*4 +3
+    //std::vector<Point> strong_adj = {Point(0,2),Point(0,5), Point(2,7),Point(5,7), Point(7,2),Point(7,5), Point(2,0),Point(5,0)};
+    std::vector<int>pcr_stable = {0,0,0,0};
+    std::vector<int>oppcr_stable = {0,0,0,0};
+    std::vector<Point> danger = {Point(1,0),Point(1,1),Point(0,1), Point(0,6),Point(1,6),Point(1,7), Point(6,0),Point(6,1),Point(7,1), Point(7,6),Point(6,6),Point(6,7)};
     int h[8][8] =
     {{20,-3,11,8,8,11,-3,20},
      {-3,-7,-4,-1,-1,-4,-7,-3},
@@ -280,7 +289,7 @@ int state(OthelloBoard st)
      {11,-4,2,2,2,2,-4,11},
      {-3,-7,-4,-1,-1,-4,-7,-3},
      {20,-3, 11,8,8,11,-3,20}};
-    int score = 0;
+    double score = 0;
     st.disc_count[st.BLACK] = st.disc_count[st.WHITE] = 0;
     st.disc_count[st.EMPTY] = 64;
     st.cur_player = player;
@@ -304,6 +313,145 @@ int state(OthelloBoard st)
             }
         }
     }
+    //enforce corner, and promote danger sect if corner is true
+    for (int i=0; i<4; i++){
+        int row = stable[i].x;
+        int col = stable[i].y;
+        if (st.board[row][col] == st.cur_player){
+            pcr_stable[i] = 1;
+            for (int j=0; j<3; j++){
+                int d_row = danger[i*3+j].x;
+                int d_col = danger[i*3+j].y;
+                if (st.board[d_row][d_col] == st.cur_player){
+                    st.disc_count[st.cur_player]++;  //equates as having more discs, later translated into scores
+                }
+            }
+        }
+        else if (st.board[row][col] == oppo.cur_player){
+            oppcr_stable[i] = 1;
+            for (int j=0; j<3; j++){
+                int d_row = danger[i*3+j].x;
+                int d_col = danger[i*3+j].y;
+                if (st.board[d_row][d_col] == oppo.cur_player){
+                    st.disc_count[oppo.cur_player]++;
+                }
+            }
+        }
+    }
+    //adjacent enforced
+    int top,left,right,bottom;
+    int top_n, left_n, right_n, bottom_n;
+    top = left = right = bottom = 1;
+    top_n = left_n = right_n = bottom_n = 0;
+
+    int op_top, op_left, op_right, op_bottom;
+    int optop_n, opleft_n, opright_n, opbottom_n;
+    op_top = op_left = op_right = op_bottom = 1;
+    optop_n = opleft_n = opright_n =opbottom_n = 0;
+    for (int k=0; k<8; k++){
+        //top l
+        if (st.board[0][k] != st.EMPTY){
+            if (st.board[0][k] == st.cur_player){
+                op_top = 0;
+                top_n++;
+            }
+            else if (st.board[0][k] == oppo.cur_player) {
+                top = 0;
+                optop_n++;
+            }
+        }
+        //
+//        if (st.board[k][0] != oppo.cur_player && st.board[k][0] != st.EMPTY){
+//            left = 0;
+//        }
+//        else left_n++;
+
+        if (st.board[k][0] != st.EMPTY){
+            if (st.board[k][0] == st.cur_player){
+                op_left = 0;
+                left_n++;
+            }
+            else if (st.board[k][0] == oppo.cur_player) {
+                left = 0;
+                opleft_n++;
+            }
+        }
+        //
+//        if (st.board[k][7] != oppo.cur_player && st.board[k][7] != st.EMPTY){
+//            right = 0;
+//        }
+//        else right_n++;
+
+        if (st.board[k][7] != st.EMPTY){
+            if (st.board[k][7] == st.cur_player){
+                op_right = 0;
+                right_n++;
+            }
+            else if (st.board[k][7] == oppo.cur_player) {
+                right = 0;
+                opright_n++;
+            }
+        }
+        //
+//        if (st.board[7][k] != oppo.cur_player && st.board[7][k] != st.EMPTY){
+//            bottom = 0;
+//        }
+//        else bottom_n++;
+
+        if (st.board[7][k] != st.EMPTY){
+            if (st.board[7][k] == st.cur_player){
+                op_bottom = 0;
+                bottom_n++;
+            }
+            else if (st.board[7][k] == oppo.cur_player) {
+                bottom = 0;
+                opbottom_n++;
+            }
+        }
+    }
+    int adj_enforced = 0;
+    //player
+    if (top){
+        adj_enforced += top_n;
+    }
+    if (left){
+        adj_enforced += left_n;
+    }
+    if (right){
+        adj_enforced += right_n;
+    }
+    if (bottom){
+        adj_enforced += bottom_n;
+    }
+    //opp
+    if (op_top){
+        adj_enforced -= optop_n;
+    }
+    if (op_left){
+        adj_enforced -= opleft_n;
+    }
+    if (op_right){
+        adj_enforced -= opright_n;
+    }
+    if (op_bottom){
+        adj_enforced -= opbottom_n;
+    }
+    //
+    int pcr_num = pcr_stable[0]+pcr_stable[1]+pcr_stable[2]+pcr_stable[3];
+    int oppcr_num = oppcr_stable[0]+oppcr_stable[1]+oppcr_stable[2]+oppcr_stable[3];
+    //find difference possession of total board state/ game control
+    int corner_diff, moves, disc_diff, res;
+    if (pcr_num+oppcr_num != 0){
+        corner_diff = (100*(pcr_num - oppcr_num))/(pcr_num+oppcr_num);
+    }
+    if (st.get_valid_spots().size() + oppo.get_valid_spots().size()!= 0){
+        moves = (100*(st.get_valid_spots().size() - oppo.get_valid_spots().size()))/(st.get_valid_spots().size() + oppo.get_valid_spots().size());
+    }
+    if (st.disc_count[st.cur_player] + st.disc_count[oppo.cur_player] != 0){
+        disc_diff = (100*(st.disc_count[st.cur_player] - st.disc_count[oppo.cur_player]))/(st.disc_count[st.cur_player] + st.disc_count[oppo.cur_player]);
+    }
+    res = 20*corner_diff + 3*moves + 2*disc_diff + 2*adj_enforced;
+    score += res;
     return score;
 }
 
